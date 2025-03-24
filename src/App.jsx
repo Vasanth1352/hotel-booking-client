2941bd17343e19dacb40bde59db76bbcf50f0023
Sukasi Vasanth <153751697+Vasanth1352@users.noreply.github.com>
2025-03-24 16:21:46 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hotels from './components/Hotels';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hotel Booking Dashboard</h1>
+      <Hotels />
+    </div>
+  );
+};
+
+export default App;
